549917904cde3020ee9017532716eda2a9183218
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-04-29 13:47:03 +0100

    Provided initial implementation of BayesLinear
    This initial implementation of `BayesLinear` acts as a wrapper for `nn.Linear`` and samples the weights from specified distributions.
    
    The log likelihoods of the prior / posterior are tracked throughout so they can be used in the loss computation.
    
    Resolves: #4

---
+++ torchwu/bayes_linear.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+
+from .base_bayesian import BayesianModule
+from .samplers.gaussian_variational import GaussianVariational
+from .samplers.scale_mixture import ScaleMixture
+
+
+class BayesLinear(BayesianModule):
+
+    def __init__(self,
+                 in_features: int,
+                 out_features: int,
+                 prior_pi: Optional[float] = 0.5,
+                 prior_sigma1: Optional[float] = 1.0,
+                 prior_sigma2: Optional[float] = 0.005) -> None:
+
+        super().__init__()
+
+        mu = torch.empty(in_features, out_features).uniform_(-0.2, 0.2)
+        rho = torch.empty(in_features, out_features).uniform_(-5.0, -4.0)
+
+        self.prior = ScaleMixture(prior_pi, prior_sigma1, prior_sigma2)
+        self.posterior = GaussianVariational(mu, rho)
+
+        self.log_prior = 0.0
+        self.log_posterior = 0.0
+
+    def forward(self, x: Tensor) -> Tensor:
+
+        w = self.posterior.sample()
+
+        self.log_prior = self.prior.log_prior(w)
+        self.log_posterior = self.posterior.log_posterior()
+
+        return F.linear(x, w)
